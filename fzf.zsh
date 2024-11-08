77cc3d2b09a29f33926eb14286d48d118de7fc98
Daniel Bast <2790401+dbast@users.noreply.github.com>
2024-11-08 19:25:50 +0100

    Load fzf completion from binary

--- fzf.zsh
+++ fzf.zsh
@@ -1,10 +1,4 @@
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source ~/.myzsh/fzf/shell/completion.zsh 2> /dev/null
-
-# Key bindings
-# ------------
-source ~/.myzsh/fzf/shell/key-bindings.zsh
+eval "$(fzf --zsh)"
 
 # Use fd-find for more speed
 if type fd &>/dev/null; then
